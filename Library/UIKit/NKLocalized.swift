77af5c77205c3cd0fd64c6cfc90967f3f9e2ded3
Nghia Nguyen <nghianp@fastsell.vn>
2016-10-11 13:32:46 +0700

    added NKLocalized to R.swift.Library

---
+++ Library/UIKit/NKLocalized.swift
@@ -0,0 +1,53 @@
+//
+//  NKLocalized.swift
+//  NKit
+//
+//  Created by Nghia Nguyen on 10/2/16.
+//  Copyright © 2016 Nghia Nguyen. All rights reserved.
+//
+
+import Foundation
+
+public final class NKLocalized: AnyObject {
+  public static let kLocalizedChangedNotificationName = "nkit_localied_changed_notification_name"
+  
+  private struct Constant {
+    static var languageCodeKey: String {return "nkit_language_code_key"}
+  }
+  
+  public static var currentLanguageCode: String? {
+    get {
+      return NSUserDefaults.standardUserDefaults().stringForKey(Constant.languageCodeKey)
+    }
+    
+    set {
+      NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: Constant.languageCodeKey)
+      NSUserDefaults.standardUserDefaults().synchronize()
+      
+      NSNotificationCenter.defaultCenter().postNotificationName(kLocalizedChangedNotificationName, object: newValue)
+    }
+  }
+  
+  public static var bundle: NSBundle = NSBundle.mainBundle()
+  
+  public static func localized(string: String, table: String? = nil) -> String {
+    if let languageCode = self.currentLanguageCode, path = self.bundle.pathForResource(languageCode, ofType: "lproj"), b = NSBundle(path: path) {
+      
+      let result = b.localizedStringForKey(string, value: nil, table: table)
+      
+      if result != string || languageCode == "en" {
+        return result
+      }
+    }
+    
+    return NSLocalizedString(string, tableName: table, bundle: self.bundle, value: "", comment: "")
+  }
+  
+  public static func availableLanguages(excludeBase: Bool = true) -> [String] {
+    var availableLanguages = self.bundle.localizations
+    if let indexOfBase = availableLanguages.indexOf("Base") where excludeBase == true {
+      availableLanguages.removeAtIndex(indexOfBase)
+    }
+    return availableLanguages
+  }
+}
